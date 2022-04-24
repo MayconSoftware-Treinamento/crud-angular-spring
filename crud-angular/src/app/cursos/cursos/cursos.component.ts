e47d7c955b000fe5895ec50e67f548e7e401207f
Maycon Santana <maycon_gus2009@hotmail.com>
2022-04-24 14:57:46 -0300

    Lista de Cursos: Tratamento de Erros e MatDialog

--- crud-angular/src/app/cursos/cursos/cursos.component.ts
+++ crud-angular/src/app/cursos/cursos/cursos.component.ts
@@ -1,27 +1,39 @@
-import { CursosService } from './../services/cursos.service';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Curso } from './../model/curso';
-import { Observable } from 'rxjs';
+import { CursosService } from './../services/cursos.service';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.scss']
+  styleUrls: ['./cursos.component.scss'],
 })
 export class CursosComponent implements OnInit {
-
   cursos$: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
-  constructor(private cursosService: CursosService) {
+  constructor(
+    private cursosService: CursosService,
+    public dialog: MatDialog) {
 
-    this.cursos$ = this.cursosService.list();
-
+      this.cursos$ = this.cursosService.list().pipe(
+      catchError((error) => {
+        this.onError('Não foi possível carregar os cursos disponíveis.');
+        return of([]);
+      })
+    );
   }
 
-  ngOnInit(): void {
-
+  // Erro ao realizar o carregamento de cursos.
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
+  ngOnInit(): void {}
 }
